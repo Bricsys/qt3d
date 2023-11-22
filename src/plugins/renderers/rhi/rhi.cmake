d685e8f3e0ac96f259554aadd7173fe199109db0
Tarja Sundqvist <tarja.sundqvist@qt.io>
2023-11-22 10:57:46 +0200

    Remove commercial SPDX-License-Identifier from build files
    The build files should not include a commercial SPDX-License-Identifier.
    Instead, a BSD-3-Clause license identifier is used.
    
    Task-number: QTQAINFRA-5900
    Change-Id: I7c33ebf44abc3d13c957d0572087286a911dcd49
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/plugins/renderers/rhi/rhi.cmake
+++ src/plugins/renderers/rhi/rhi.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial
+# SPDX-License-Identifier: BSD-3-Clause
 
 function(qt3d_setup_rhi_renderer_target target)
 
